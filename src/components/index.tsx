165c486c473e8107bccef1d92521f1c821c777ff
Blessing Krofegha <bkrofegha@gmail.com>
2020-04-23 03:02:13 +0200

    add title to index file

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,9 +2,8 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 
 import { HelloWorld } from "./App";
-import './index.css'
 
 ReactDOM.render(
   <HelloWorld userName="Beveloper" lang="TypeScript" />,
-  document.getElementById("content")
+  document.getElementById("output")
 );
